5ae92030154a9ed5266897d0dbc9cacb930226f9
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-08-30 12:56:27 +0200

    Don't uglify
    That's up to the user.

--- dist/travis-status.js
+++ dist/travis-status.js
@@ -1,1 +1,71 @@
-"use strict";var _createClass=function(){function t(t,e){for(var r in e){var n=e[r];n.configurable=!0,n.value&&(n.writable=!0)}Object.defineProperties(t,e)}return function(e,r,n){return r&&t(e.prototype,r),n&&t(e,n),e}}(),_inherits=function(t,e){if("function"!=typeof e&&null!==e)throw new TypeError("Super expression must either be null or a function, not "+typeof e);t.prototype=Object.create(e&&e.prototype,{constructor:{value:t,enumerable:!1,writable:!0,configurable:!0}}),e&&(t.__proto__=e)},_classCallCheck=function(t,e){if(!(t instanceof e))throw new TypeError("Cannot call a class as a function")},TravisStatus=function(t){function e(){_classCallCheck(this,e),null!=t&&t.apply(this,arguments)}return _inherits(e,t),_createClass(e,{username:{value:function(){var t=this.getAttribute("username");if(!t)throw new Error("A username is required");return t}},repo:{value:function(){var t=this.getAttribute("repo");if(!t)throw new Error("A repo is required");return t}},branch:{value:function(){return this.getAttribute("branch")||"master"}},imageURL:{value:function(){return"https://api.travis-ci.org/"+this.username()+"/"+this.repo()+".svg?branch="+this.branch()}},createdCallback:{value:function(){this.setAttribute("src",this.imageURL())}},attributeChangedCallback:{value:function(){this.setAttribute("src",this.imageURL())}}}),e}(HTMLImageElement);document.registerElement("travis-status",{prototype:TravisStatus.prototype,"extends":"img"});
+"use strict";
+
+var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _inherits = function (subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
+
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+var TravisStatus = (function (_HTMLImageElement) {
+	function TravisStatus() {
+		_classCallCheck(this, TravisStatus);
+
+		if (_HTMLImageElement != null) {
+			_HTMLImageElement.apply(this, arguments);
+		}
+	}
+
+	_inherits(TravisStatus, _HTMLImageElement);
+
+	_createClass(TravisStatus, {
+		username: {
+			value: function username() {
+				var usernameAttr = this.getAttribute("username");
+
+				if (!usernameAttr) {
+					throw new Error("A username is required");
+				}
+
+				return usernameAttr;
+			}
+		},
+		repo: {
+			value: function repo() {
+				var repoAttr = this.getAttribute("repo");
+
+				if (!repoAttr) {
+					throw new Error("A repo is required");
+				}
+
+				return repoAttr;
+			}
+		},
+		branch: {
+			value: function branch() {
+				return this.getAttribute("branch") || "master";
+			}
+		},
+		imageURL: {
+			value: function imageURL() {
+				return "https://api.travis-ci.org/" + this.username() + "/" + this.repo() + ".svg?branch=" + this.branch();
+			}
+		},
+		createdCallback: {
+			value: function createdCallback() {
+				this.setAttribute("src", this.imageURL());
+			}
+		},
+		attributeChangedCallback: {
+			value: function attributeChangedCallback() {
+				this.setAttribute("src", this.imageURL());
+			}
+		}
+	});
+
+	return TravisStatus;
+})(HTMLImageElement);
+
+document.registerElement("travis-status", {
+	prototype: TravisStatus.prototype,
+	"extends": "img"
+});
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,6 @@
 'use strict';
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import uglify from 'gulp-uglify';
 import wct from 'web-component-tester';
 
 wct.gulp.init(gulp);
@@ -9,6 +8,5 @@ wct.gulp.init(gulp);
 gulp.task('build', () => {
 	return gulp.src('src/travis-status.js')
 		.pipe(babel())
-		.pipe(uglify())
 		.pipe(gulp.dest('dist/'));
 });
